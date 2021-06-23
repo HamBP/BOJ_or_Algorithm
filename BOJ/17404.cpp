9c230140520d4675c5d2bcd358ebc18b1846008b
HamBP <35232655+HamBP@users.noreply.github.com>
2021-06-23 23:47:19 +0900

    repeat to for
    반복되는 부분을 극한의 for 문으로 교체

--- BOJ/17404.cpp
+++ BOJ/17404.cpp
@@ -1,79 +1,50 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <vector>
 #include <algorithm>
-#include <queue>
-#include <string>
-#include <cmath>
-#include <set>
-#include <map>
-#include <numeric>
-#include <functional>
-#include <stack>
-#include <math.h>
 
 using namespace std;
-
-typedef long long ll;
-
-int dx[] = { 0, 0, -1, 1, 1, 1, -1, -1 };
-int dy[] = { -1, 1, 0, 0, -1, 1, 1, -1 };
-
-const int INF = 987654321;
-
-/* --- */
 
 int rgb[3][3];
 int n;
 
 int main() {
+    int in[3];
+
     cin >> n;
 
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    rgb[0][0] = a;
-    rgb[1][1] = b;
-    rgb[2][2] = c;
-    rgb[0][1] = rgb[0][2] = rgb[1][0] = rgb[1][2] = rgb[2][0] = rgb[2][1] = 1000;
+    cin >> rgb[0][0] >> rgb[1][1] >> rgb[2][2];
+
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (i != j) rgb[i][j] = 1000;
 
     for (int i = 1; i < n - 1; ++i) {
-        int a, b, c;
-        scanf("%d %d %d", &a, &b, &c);
+        scanf("%d %d %d", &in[0], &in[1], &in[2]);
 
-        int aa, ab, ac, ba, bb, bc, ca, cb, cc;
+        for (int j = 0; j < 3; ++j) {
+            int x[3];
+            for (int k = 0; k < 3; ++k) {
+                x[k] = 1000 * 1000;
+                for (int h = 0; h < 3; ++h) 
+                    if(k != h) 
+                        x[k] = min(x[k], in[k] + rgb[j][h]);
+            }
 
-        aa = a + min(rgb[0][1], rgb[0][2]);
-        ab = b + min(rgb[0][0], rgb[0][2]);
-        ac = c + min(rgb[0][0], rgb[0][1]);
-
-        ba = a + min(rgb[1][1], rgb[1][2]);
-        bb = b + min(rgb[1][0], rgb[1][2]);
-        bc = c + min(rgb[1][0], rgb[1][1]);
-
-        ca = a + min(rgb[2][1], rgb[2][2]);
-        cb = b + min(rgb[2][0], rgb[2][2]);
-        cc = c + min(rgb[2][0], rgb[2][1]);
-
-        rgb[0][0] = aa;
-        rgb[0][1] = ab;
-        rgb[0][2] = ac;
-
-        rgb[1][0] = ba;
-        rgb[1][1] = bb;
-        rgb[1][2] = bc;
-
-        rgb[2][0] = ca;
-        rgb[2][1] = cb;
-        rgb[2][2] = cc;
+            for (int k = 0; k < 3; ++k) rgb[j][k] = x[k];
+        }
     }
 
-    scanf("%d %d %d", &a, &b, &c);
-    a += min({ rgb[1][1], rgb[1][2], rgb[2][1], rgb[2][2] });
-    b += min({ rgb[0][0], rgb[0][2], rgb[2][0], rgb[2][2] });
-    c += min({ rgb[0][0], rgb[0][1], rgb[1][0], rgb[1][1] });
+    int m = 1000 * 1000;
 
-    cout << min({a, b, c});
+    scanf("%d %d %d", &in[0], &in[1], &in[2]);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                if (i != k && j != k) m = min(m, in[k] + rgb[i][j]);
+            }
+        }
+    }
+
+    cout << m;
 
     return 0;
 }
